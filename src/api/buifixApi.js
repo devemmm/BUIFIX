f1e9c26110fae23f7006a6e31580081be83ee202
devemmm <djntivuguruzwaemmanuel@gmail.com>
2023-05-29 09:17:49 +0200

    changing API url

--- src/api/buifixApi.js
+++ src/api/buifixApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://185.193.67.94:9000',
+    baseURL: 'https://buifix.yocast.rw',
     // baseURL: 'https://buifix-api.herokuapp.com'
     // baseURL: 'https://4df7-105-178-110-233.eu.ngrok.io',
     // baseURL: 'http://localhost:3000'
